73d4654352fdd477bc6481c0b21fdc159c31b738
Joey Nelson <joey@wlrs.net>
2011-11-11 12:22:25 -0500

    redirect http

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?
 
 // comment this out to allow http:
-// if($_SERVER['SERVER_PORT'] == 80){
-// 	header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-// 	die();
-// }
+if($_SERVER['SERVER_PORT'] == 80){
+	header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+	die();
+}
 
 $key_publishable = '';
 $key_secret = '';
