5f74697fac9213e49bc561473381f7d194e0273a
Joey Nelson <joey@wlrs.net>
2011-11-18 19:42:01 -0500

    ocd cleanup (sorry)

--- form.js
+++ form.js
@@ -1,163 +1,163 @@
 function show_error(input, message){
-	$('#input_' + input).addClass('error');
-	$('#error_' + input).html(message).show();
-	return false
+    $('#input_' + input).addClass('error');
+    $('#error_' + input).html(message).show();
+    return false
 }
 
 function clear_error(input){
-	$('#input_' + input).removeClass('error');
-	$('#error_' + input).html('').hide();
+    $('#input_' + input).removeClass('error');
+    $('#error_' + input).html('').hide();
 }
 
 function lock_inputs(){
-	$('input').attr('disabled', 'disabled');
-	$('#submit_button').attr('disabled', 'disabled');
-	$('#transaction_error').hide();
+    $('input').attr('disabled', 'disabled');
+    $('#submit_button').attr('disabled', 'disabled');
+    $('#transaction_error').hide();
 }
 
 function unlock_inputs(){
-	$('input').removeAttr('disabled');
-	$('#submit_button').removeAttr('disabled');
-	$('#progress_message').hide();
-}			
+    $('input').removeAttr('disabled');
+    $('#submit_button').removeAttr('disabled');
+    $('#progress_message').hide();
+}           
 
 function validate(field, value){
-	if(field == 'number'){
-		if(!value.length) return show_error(field, 'Please enter a credit card number');
-	}
+    if(field == 'number'){
+        if(!value.length) return show_error(field, 'Please enter a credit card number');
+    }
 
-	if(field == 'cvc'){
-		if(!value.length) return show_error(field, 'Please enter a CVC');
-	}
+    if(field == 'cvc'){
+        if(!value.length) return show_error(field, 'Please enter a CVC');
+    }
 
-	if(field == 'exp_month'){
-		if(!value.length) return show_error(field, 'Please enter a month');
-		if(value < 1 || value > 12) return show_error(field, 'Month should be between 1 and 12');
-	}					
+    if(field == 'exp_month'){
+        if(!value.length) return show_error(field, 'Please enter a month');
+        if(value < 1 || value > 12) return show_error(field, 'Month should be between 1 and 12');
+    }                   
 
-	if(field == 'exp_year'){
-		if(!value.length) return show_error(field, 'Please enter a year');
-	}
+    if(field == 'exp_year'){
+        if(!value.length) return show_error(field, 'Please enter a year');
+    }
 
-	if(field == 'amount'){
-		if(!value.length) return show_error(field, 'Please enter an amount');
-		if(value < 0.50) return show_error(field, 'Minimum charge is $0.50');
-	}
+    if(field == 'amount'){
+        if(!value.length) return show_error(field, 'Please enter an amount');
+        if(value < 0.50) return show_error(field, 'Minimum charge is $0.50');
+    }
 
-	clear_error(field);
-	return true;
+    clear_error(field);
+    return true;
 }
 
 function sanitize(field){
-	var input = $('#input_' + field);
-	var value = $.trim(input.val());
+    var input = $('#input_' + field);
+    var value = $.trim(input.val());
 
-	if(field == 'number'){
-		value = value.replace(/[^\d]+/g, '');
-	}
+    if(field == 'number'){
+        value = value.replace(/[^\d]+/g, '');
+    }
 
-	if(field == 'cvc'){
-		value = value.replace(/[^\d]+/g, '');	
-	}
+    if(field == 'cvc'){
+        value = value.replace(/[^\d]+/g, '');   
+    }
 
-	if(field == 'exp_month'){
-		value = value.replace(/[^\d]+/g, '');
-		value = value.replace(/^0+/, '');
-	}
+    if(field == 'exp_month'){
+        value = value.replace(/[^\d]+/g, '');
+        value = value.replace(/^0+/, '');
+    }
 
-	if(field == 'exp_year'){
-		value = value.replace(/[^\d]+/g, '');
-		if(value.length == 2) value = '20' + value;
-	}
+    if(field == 'exp_year'){
+        value = value.replace(/[^\d]+/g, '');
+        if(value.length == 2) value = '20' + value;
+    }
 
-	if(field == 'amount'){
-		value = value.replace(/[^\d\.]+/g, '');
-		if(value.length) value = parseFloat(value).toFixed(2);
-	}
+    if(field == 'amount'){
+        value = value.replace(/[^\d\.]+/g, '');
+        if(value.length) value = parseFloat(value).toFixed(2);
+    }
 
-	input.val(value);
+    input.val(value);
 }
 
 
 $(function(){
-	$('input').attr('autocomplete', 'off');
+    $('input').attr('autocomplete', 'off');
 
-	unlock_inputs();
+    unlock_inputs();
 
-	$('input').blur(function(){
-		var input = $(this);
-		var name = input.attr('id').replace(/^input_/, '');
+    $('input').blur(function(){
+        var input = $(this);
+        var name = input.attr('id').replace(/^input_/, '');
 
-		sanitize(name);
-		validate(name, input.val());
-	});
+        sanitize(name);
+        validate(name, input.val());
+    });
 
-	$('input').keydown(function(){
-		$('#transaction_error').hide();
-	});
+    $('input').keydown(function(){
+        $('#transaction_error').hide();
+    });
 
-	$('#payment_form').submit(function(){
-		$('input').blur(); //trigger sanitize/validate functions
+    $('#payment_form').submit(function(){
+        $('input').blur(); //trigger sanitize/validate functions
 
-		if($('input.error').length > 0){
-			unlock_inputs()
-			$('input.error:first').focus();
-			return false;
-		}
+        if($('input.error').length > 0){
+            unlock_inputs()
+            $('input.error:first').focus();
+            return false;
+        }
 
-		lock_inputs();
+        lock_inputs();
 
-		var params = {};
-		params['number'] = $('#input_number').val();
-		params['cvc'] = $('#input_cvc').val();
-		params['exp_month'] = $('#input_exp_month').val();
-		params['exp_year'] = $('#input_exp_year').val();
+        var params = {};
+        params['number']    = $('#input_number').val();
+        params['cvc']       = $('#input_cvc').val();
+        params['exp_month'] = $('#input_exp_month').val();
+        params['exp_year']  = $('#input_exp_year').val();
 
-		var amount = $('#input_amount').val().replace(/\./g, '');
+        var amount = $('#input_amount').val().replace(/\./g, '');
 
-		$('#progress_message').html('Validating card data...').show();
+        $('#progress_message').html('Validating card data...').show();
 
-		Stripe.createToken(params, amount, function(status, response){				
-			if(response.error){
-				$('#transaction_error').html(response.error.message).slideDown();
-				unlock_inputs();
-				return false;
-			}
+        Stripe.createToken(params, amount, function(status, response){              
+            if(response.error){
+                $('#transaction_error').html(response.error.message).slideDown();
+                unlock_inputs();
+                return false;
+            }
 
-			var charge = {};
-			charge['token'] = response['id'];
-			charge['amount'] = amount;
-			charge['note'] = $('#input_note').val();
-			$('#progress_message').html('Submitting charge...').show();
-			$.post(location.href, charge, function(response){
+            var charge = {};
+            charge['token'] = response['id'];
+            charge['amount'] = amount;
+            charge['note'] = $('#input_note').val();
+            $('#progress_message').html('Submitting charge...').show();
+            $.post(location.href, charge, function(response){
 
-				try{
-					response = JSON.parse(response);
-				}catch(err){
-					//first way to fail: server returns something that's not json
-					if(!$.trim(response).length){
-						response = {error: 'Server returned empty response during charge attempt'};
-					}else{
-						response = {error: 'Server returned invalid response:<br /><br />' + response};
-					}
-				}
-				
-				if(response['success']){
-					$('#wrap').html("<h2>MISSION ACCOMPLISHED</h2><b>$" + response['amount'] + " is making its way to our bank account.</b><br />Transaction ID: " + response['id'] + "<br /><br />").css('background-color', '#fff');
-					$("<a href='javascript:void(0);' class='red'>Make another charge</a>").click(function(){ location.href = location.href; }).appendTo('#wrap');
-				}else{
-					//second way to fail: stripe declined the charge
-					$('#transaction_error').html(response['error']).slideDown();
-					unlock_inputs();
-				}
-			}).error(function(response){
-				//third way to fail: server responds with an error code
-				$('#transaction_error').html('Error on charge attempt:<br /><br />' + response.responseText).slideDown();
-				unlock_inputs();
-			});
-		
-		});
+                try{
+                    response = JSON.parse(response);
+                }catch(err){
+                    //first way to fail: server returns something that's not json
+                    if(!$.trim(response).length){
+                        response = {error: 'Server returned empty response during charge attempt'};
+                    }else{
+                        response = {error: 'Server returned invalid response:<br /><br />' + response};
+                    }
+                }
+                
+                if(response['success']){
+                    $('#wrap').html("<h2>MISSION ACCOMPLISHED</h2><b>$" + response['amount'] + " is making its way to our bank account.</b><br />Transaction ID: " + response['id'] + "<br /><br />").css('background-color', '#fff');
+                    $("<a href='javascript:void(0);' class='red'>Make another charge</a>").click(function(){ location.href = location.href; }).appendTo('#wrap');
+                }else{
+                    //second way to fail: stripe declined the charge
+                    $('#transaction_error').html(response['error']).slideDown();
+                    unlock_inputs();
+                }
+            }).error(function(response){
+                //third way to fail: server responds with an error code
+                $('#transaction_error').html('Error on charge attempt:<br /><br />' + response.responseText).slideDown();
+                unlock_inputs();
+            });
+        
+        });
 
-		return false;
-	});
+        return false;
+    });
 });
--- index.php
+++ index.php
@@ -2,146 +2,146 @@
 
 // comment this out to allow http:
 if($_SERVER['SERVER_PORT'] == 80){
-	header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-	die();
+    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    die();
 }
 
-$key_publishable = '';
-$key_secret = '';
-$note_prefix = 'Terminal'; //this will be used in the charge's description
-$title = 'Terminal';
-$currency = 'usd';
-$currency_symbol = '$';
-$demo_mode = false;
+$key_publishable    = '';
+$key_secret         = '';
+$note_prefix        = 'Terminal'; //this will be used in the charge's description
+$title              = 'Terminal';
+$currency           = 'usd';
+$currency_symbol    = '$';
+$demo_mode          = false;
 
 if(!$key_publishable || !$key_secret) die('Please set stripe API keys');
 
 if($_POST){
-	require_once 'stripe-php/lib/Stripe.php';
+    require_once 'stripe-php/lib/Stripe.php';
 
-	$note_parts = array();
-	if($note_prefix) $note_parts[] = $note_prefix;
-	if($_POST['note']) $note_parts[] = $_POST['note'];
+    $note_parts = array();
+    if($note_prefix) $note_parts[] = $note_prefix;
+    if($_POST['note']) $note_parts[] = $_POST['note'];
 
-	$params = array(
-		'amount' => $_POST['amount'],
-		'currency' => $currency,
-		'card' => $_POST['token'],
-		'description' => implode(' - ', $note_parts)
-	);
+    $params = array(
+        'amount'        => $_POST['amount'],
+        'currency'      => $currency,
+        'card'          => $_POST['token'],
+        'description'   => implode(' - ', $note_parts)
+    );
 
-	$response = array(
-		'success' => false
-	);
+    $response = array(
+        'success' => false
+    );
 
-	try{
-		Stripe::setApiKey($key_secret);
-		$charge = Stripe_Charge::create($params);
+    try{
+        Stripe::setApiKey($key_secret);
+        $charge = Stripe_Charge::create($params);
 
-		$response['success'] = true;
-		$response['id'] = $charge->id;
-		$response['amount'] = number_format($charge->amount / 100, 2);
-		$response['fee'] = number_format($charge->fee / 100, 2);
-		$response['card_type'] = $charge->card->type;
-		$response['card_last4'] = $charge->card->last4;
-	}catch (Exception $e) {
-		$response['error'] = $e->getMessage();
-	}
+        $response['success']    = true;
+        $response['id']         = $charge->id;
+        $response['amount']     = number_format($charge->amount / 100, 2);
+        $response['fee']        = number_format($charge->fee / 100, 2);
+        $response['card_type']  = $charge->card->type;
+        $response['card_last4'] = $charge->card->last4;
+    }catch (Exception $e) {
+        $response['error'] = $e->getMessage();
+    }
 
-	echo json_encode($response);
-	die();
+    echo json_encode($response);
+    die();
 }
 
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?= $title ?></title>
-		<link rel="stylesheet" type="text/css" media="screen" href="layout.css?<?= filemtime('layout.css') ?>" />
-		<meta name="viewport" content="width=480px" />
-		<meta name="viewport" content="user-scalable=no" />
-	</head>
-	<body>
-		<div id="wrap">
-			<h2><?= $title ?></h2>
+    <head>
+        <title><?= $title ?></title>
+        <link rel="stylesheet" type="text/css" media="screen" href="layout.css?<?= filemtime('layout.css') ?>" />
+        <meta name="viewport" content="width=480px" />
+        <meta name="viewport" content="user-scalable=no" />
+    </head>
+    <body>
+        <div id="wrap">
+            <h2><?= $title ?></h2>
 
-			<? if($demo_mode){ ?>
-				<div id="demo_warning">
-					This is a <b>DEMO</b>. Please don't enter real payment info. You can use <b>4242424242424242</b> as a valid card number. <a href="https://stripe.com/docs/testing">Stripe Testing FAQ</a>
-				</div>
-			<? } ?>
+            <? if($demo_mode){ ?>
+                <div id="demo_warning">
+                    This is a <b>DEMO</b>. Please don't enter real payment info. You can use <b>4242424242424242</b> as a valid card number. <a href="https://stripe.com/docs/testing">Stripe Testing FAQ</a>
+                </div>
+            <? } ?>
 
-			<form action="" method="POST" id="payment_form">
-			    
-				<label>Amount</label>
+            <form action="" method="POST" id="payment_form">
+                
+                <label>Amount</label>
 
-				<div class="form_input">
-					<input id="input_amount" type="text" placeholder="Ex: <?=$currency_symbol?>19.99" />
-				</div>
-				
-				<div class="clear"></div>
-				<div id="error_amount" class="error_wrapper"></div>
-
-
-				<label>Card Number</label>
-
-				<div class="form_input">
-					<input id="input_number" type="text" pattern="[0-9]*"/>
-				</div>
-				
-				<div class="clear"></div>
-				<div id="error_number" class="error_wrapper"></div>
+                <div class="form_input">
+                    <input id="input_amount" type="text" placeholder="Ex: <?=$currency_symbol?>19.99" />
+                </div>
+                
+                <div class="clear"></div>
+                <div id="error_amount" class="error_wrapper"></div>
 
 
-				<label>CVC</label>
+                <label>Card Number</label>
 
-				<div class="form_input">
-					<input id="input_cvc" type="text" pattern="[0-9]*" />
-				</div>
-
-				<div class="clear"></div>
-				<div id="error_cvc" class="error_wrapper"></div>
-
-			    
-			    <label>Expiration</label>
-
-			    <div class="form_input">
-			    	<input id="input_exp_month" type="text" pattern="[0-9]*" placeholder="MM" />
-			        <span> / </span>
-			        <input id="input_exp_year" type="text" pattern="[0-9]*" placeholder="YYYY" />
-			    </div>
-			        
-				<div class="clear"></div>
-				<div id="error_exp_month" class="error_wrapper"></div>
-				<div id="error_exp_year" class="error_wrapper"></div>
+                <div class="form_input">
+                    <input id="input_number" type="text" pattern="[0-9]*"/>
+                </div>
+                
+                <div class="clear"></div>
+                <div id="error_number" class="error_wrapper"></div>
 
 
-				<label>Note</label>
+                <label>CVC</label>
 
-				<div class="form_input">
-					<input id="input_note" type="text" placeholder="A short note" />
-				</div>
-				
-				<div class="clear"></div>
-				<div id="error_note" class="error_wrapper"></div>
+                <div class="form_input">
+                    <input id="input_cvc" type="text" pattern="[0-9]*" />
+                </div>
+
+                <div class="clear"></div>
+                <div id="error_cvc" class="error_wrapper"></div>
+
+                
+                <label>Expiration</label>
+
+                <div class="form_input">
+                    <input id="input_exp_month" type="text" pattern="[0-9]*" placeholder="MM" />
+                    <span> / </span>
+                    <input id="input_exp_year" type="text" pattern="[0-9]*" placeholder="YYYY" />
+                </div>
+                    
+                <div class="clear"></div>
+                <div id="error_exp_month" class="error_wrapper"></div>
+                <div id="error_exp_year" class="error_wrapper"></div>
 
 
-				<div id="transaction_error"></div>
-			    <div class="left" id="progress_message"></div>
+                <label>Note</label>
 
-			    <div class="right" style="margin-top: 10px;">
-			    	<button id="submit_button" type="submit">Submit</button>
-			    </div>
+                <div class="form_input">
+                    <input id="input_note" type="text" placeholder="A short note" />
+                </div>
+                
+                <div class="clear"></div>
+                <div id="error_note" class="error_wrapper"></div>
 
-			    <div class="clear"></div>
-			</form>
-		</div>
 
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-		<script type="text/javascript" src="https://js.stripe.com/v1/"></script>
-		<script type="text/javascript" src="form.js?<?= filemtime('form.js') ?>"></script>
-		<script type="text/javascript">
-			Stripe.setPublishableKey('<?= $key_publishable ?>');
-		</script>
-	</body>
+                <div id="transaction_error"></div>
+                <div class="left" id="progress_message"></div>
+
+                <div class="right" style="margin-top: 10px;">
+                    <button id="submit_button" type="submit">Submit</button>
+                </div>
+
+                <div class="clear"></div>
+            </form>
+        </div>
+
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https://js.stripe.com/v1/"></script>
+        <script type="text/javascript" src="form.js?<?= filemtime('form.js') ?>"></script>
+        <script type="text/javascript">
+            Stripe.setPublishableKey('<?= $key_publishable ?>');
+        </script>
+    </body>
 </html>
